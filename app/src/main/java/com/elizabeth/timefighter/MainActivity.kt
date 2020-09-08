b5a8c4c5924fb874d24097d9f53ad9887c4e03a8
Elizabeth Gonzalez Cruz <elizabeth.gonzalez@paradox-tech.com>
2020-09-08 12:08:17 -0500

    inicializando funciones

--- app/src/main/java/com/elizabeth/timefighter/MainActivity.kt
+++ app/src/main/java/com/elizabeth/timefighter/MainActivity.kt
@@ -2,41 +2,74 @@ package com.elizabeth.timefighter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var gameScoreTextView: TextView
-    private lateinit var timeScoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
     private lateinit var tapMeButton: Button
     private var score = 0
+
+    //propiedades del juego
+    private var gameStarted = false
+    private lateinit var countDownTimer: CountDownTimer
+    private var initialCountDown: Long = 60000
+    private var countDownInterval: Long = 1000
+    private var timeLeft = 60
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //inicializar vistas
+        //connect views to variables
         gameScoreTextView = findViewById(R.id.game_score_text_view)
-        timeScoreTextView = findViewById(R.id.time_left_text_view)
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
         tapMeButton = findViewById(R.id.tap_me_button)
         //
-        tapMeButton.setOnClickListener{incrementScore()}
+        tapMeButton.setOnClickListener { incrementScore() }
 
-        //connect views to variables
-
+        resetGame()
     }
 
     private fun incrementScore() {
         //increment score logic
-        score ++
+        score++
 
-        val newScore = "Your Score: $score"
+        val newScore = getString(R.string.your_score, score)
         gameScoreTextView.text = newScore
     }
 
     private fun resetGame() {
-        // reset game logic
+        // 1 set the escore
+        score = 0
+
+        //create a variable to store the score as a string
+        val initialScore = getString(R.string.your_score, score)
+        gameScoreTextView.text = initialScore
+
+        val initialTimeLeft = getString(R.string.time_left, 60)
+        timeLeftTextView.text = initialTimeLeft
+
+        // 2
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            // 3
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeft = millisUntilFinished.toInt() / 1000
+
+                val timeLeftString = getString(R.string.time_left, timeLeft)
+                timeLeftTextView.text = timeLeftString
+            }
+
+            override fun onFinish() {
+                // To Be Implemented Later
+            }
+        }
+
+        // 4
+        gameStarted = false
     }
 
     private fun startGame() {
